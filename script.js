6a843e3bc5aa443b61a47d301adbf6bf729b3de3
Marshall Ku <taengoo890309@naver.com>
2019-07-24 10:56:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
 Array.from(document.querySelectorAll("iframe")).forEach(function(a) {
-  const width = a.getAttribute("width"),
-        height = a.getAttribute("height"),
+  const r = a.width / a.height,
         reswrapper = document.createElement("div");
 
-  if (width / height === 16 / 9) {
-    reswrapper.classList.add("resvid");
-    a.parentElement.insertBefore(reswrapper, a);
-    reswrapper.appendChild(a)
-  }
-  if (width / height === 9 / 16) {
-    reswrapper.classList.add("resvid", "alt");
-    a.parentElement.insertBefore(reswrapper, a);
-    reswrapper.appendChild(a)
-  }
-  if (width / height === 4 / 3) {
-    reswrapper.classList.add("resvid", "old");
-    a.parentElement.insertBefore(reswrapper, a);
-    reswrapper.appendChild(a)
-  }
-  if (width === height) {
-    reswrapper.classList.add("resvid", "square");
-    a.parentElement.insertBefore(reswrapper, a);
-    reswrapper.appendChild(a)
-  }
+  reswrapper. classList.add("resvid", `${9 / 16 === r ? "alt" : 4 / 3 === r ? "old" : r === 1 && "square"}`),
+  a.parentElement.insertBefore(reswrapper, a);
+  reswrapper.appendChild(a)
 })
