3884ced035e0f53ff11f87327c81ef54bd459509
Marshall Ku <taengoo890309@naver.com>
2019-07-31 17:28:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-Array.from(document.querySelectorAll("iframe")).forEach(function(a) {
+Array.from(document.querySelectorAll("iframe")).forEach(function (a) {
   const r = a.width / a.height,
-        reswrapper = document.createElement("div");
+    reswrapper = document.createElement("div"),
+    res = "resvid";
 
-  reswrapper.classList.add("resvid", `${9 / 16 === r ? "alt" : 4 / 3 === r ? "old" : r === 1 && "square"}`),
+  reswrapper.classList.add(`${16 / 9 === r ? res : 9 / 16 === r ? (res, "alt") : 4 / 3 === r ? (res, "old") : r === 1 && (res, "square")}`),
   a.parentElement.insertBefore(reswrapper, a);
   reswrapper.appendChild(a)
 })
